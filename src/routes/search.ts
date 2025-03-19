ec71323280b72fd381c49903cfaf8b89e1fcb6f8
Khalil Gharbi <khalilgha812@gmail.com>
2025-03-19 15:12:26 +0100

    switch to torrent agent for searching torrents

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,29 +1,17 @@
 import { Router } from "express";
-import torSearch from "torrent-search-api";
-import { Torrent } from "../types/search.js";
 import { HandlerConfig } from "../types/config.js";
+import TorrentAgent from "torrent-agent";
+import { Torrent } from "torrent-agent/dist/scrapers/scraper.js";
+import { DefaultScrapers } from "torrent-agent/dist/query.js";
 
 export function search(router: Router, config: Partial<HandlerConfig>) {
+  const agent = new TorrentAgent();
   router.get("/api/search", async (req, res) => {
     try {
-      const providers = req.query.providers;
-
-      if (!providers || typeof providers != "string") {
-        torSearch.enablePublicProviders();
-      } else {
-        torSearch.disableAllProviders();
-        let pros = providers.split(",");
-        for (let p of pros) {
-          torSearch.enableProvider(p);
-        }
-      }
-      let limit = req.query.limit;
-      if (!limit || isNaN(+limit)) {
-        limit = "20";
-      }
-      let cat = req.query.category;
-      if (!cat || typeof cat != "string") {
-        cat = "Movies";
+      let limitQ = req.query.limit;
+      let limit = 20;
+      if (limitQ && !isNaN(+limitQ)) {
+        limit = +limitQ;
       }
       let q = req.query.query;
       if (!q || typeof q != "string") {
@@ -32,19 +20,24 @@ export function search(router: Router, config: Partial<HandlerConfig>) {
         });
         return;
       }
-      const torrents = await torSearch.search(q, cat, +limit);
-      let magnets: Torrent[] = [];
+      res.setHeader("Content-Type", "text/event-stream");
+      res.setHeader("Cache-Control", "no-cache");
+      res.setHeader("Connection", "keep-alive");
+      const query = agent.add({
+        searchQuery: q,
+        options: {
+          limit: limit,
+          concurrency: 10,
+        },
+        scrapers: DefaultScrapers,
+      });
 
-      for (let torrent of torrents) {
-        const magnet = await torSearch.getMagnet(torrent);
-        const t: Torrent = {
-          ...torrent,
-          magnetURI: magnet,
-        };
-        magnets.push(t);
-      }
-
-      res.status(200).json(magnets);
+      query.on("torrent", (t) => {
+        res.write(`data: ${JSON.stringify(t)}\n\n`);
+      });
+      query.on("done", () => {
+        res.end();
+      });
     } catch (err) {
       res.status(500).json({
         error: "Internal Server Error",
